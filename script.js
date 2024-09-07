1d95100ec67daecc1024e79c805b263e2b7a1fc3
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2024-09-07 21:38:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 class Person {
-constructor(name, age) {
-  this.name = name;
-  this.age = age;
-}
-
-  getName() {
-    return this.name; 
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
   }
-
+  getName() {
+    return this.name;  // This line is correct now
+  }
   setAge(age) {
     this.age = age;
   }
@@ -27,7 +25,6 @@ class Teacher extends Person {
   constructor(name, age) {
     super(name, age);
   }
-
   teach() {
     console.log(`${this.name} is teaching`);
   }
@@ -36,6 +33,7 @@ class Teacher extends Person {
 const person = new Person("John", 30);
 const student = new Student("Alice", 22);
 const teacher = new Teacher("Bob", 40);
+
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
